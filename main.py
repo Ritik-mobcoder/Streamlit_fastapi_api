8e86bfbc6cbd95a95ae9e9e865c6504e11cb2e76
Ritik-mobcoder <ritik.gupta@mobcoder.com>
2024-11-14 16:51:51 +0530

    fixed logic

--- main.py
+++ main.py
@@ -5,13 +5,6 @@ from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
-model_path = "model.pkl"
-with open(model_path, "rb") as f:
-    model = pickle.load(f)
-
-with open("log_reg.pkl", "rb") as f:
-    model_lr = pickle.load(f)
-
 
 @app.post("/predict")
 async def root(request: Request):
@@ -19,10 +12,12 @@ async def root(request: Request):
         body = await request.json()
         experience = body["experience"]
         input_data = np.array([[experience]])
-        prediction = model.predict(input_data)
-        # print(prediction)
-        response_data = {"prediction_salary": prediction.tolist()}
-        return JSONResponse(response_data)
+        model_path = "model.pkl"
+        with open(model_path, "rb") as f:
+            model = pickle.load(f)
+            prediction = model.predict(input_data)
+            response_data = {"prediction_salary": prediction.tolist()}
+            return JSONResponse(response_data)
     except Exception as e:
         error_message = str(e)
         response_data = {"error": error_message}
@@ -34,10 +29,14 @@ async def root(request: Request):
     try:
         body = await request.json()
         pclass, sex, age, sibsp, parch, fare = body.values()
-        input_data = np.array([[pclass, sex, age, sibsp, parch, fare]])
-        prediction = model_lr(input_data)
-        return f"{prediction}"
-        response_data = {"prediction_salary": f"{prediction[0]}"}
+        input_data = np.array([[pclass, sex, age, sibsp, parch, fare]]).astype(
+            np.float64
+        )
+        with open("log_reg.pkl", "rb") as f:
+            model_lr = pickle.load(f)
+            prediction = model_lr.predict(input_data)
+            return f"{prediction}"
+    except Exception as e:
+        error_message = str(e)
+        response_data = {"error": error_message}
         return JSONResponse(response_data)
-    except Exception:
-        print("")
